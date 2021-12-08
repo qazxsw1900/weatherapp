3c01d3fc5bc613a65767c2a6c32e638550ccde7f
qazxsw1900 <66081894+qazxsw1900@users.noreply.github.com>
2021-12-08 15:15:48 +0200

    iniitial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn forecast_site.wsgi
---
+++ _main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, HttpResponse
+from django.contrib.gis.geoip2 import GeoIP2
+
+
+# Create your views here.
+def weather_page(request):
+    ip = get_client_ip(request)
+    g = GeoIP2()
+    g.city('72.14.207.99')
+    return HttpResponse('h')
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
